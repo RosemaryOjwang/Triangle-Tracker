ddfb2e0b23d729f9fdbc9500363112ca78ae8cfe
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-16 01:23:48 +0300

    add functionality to display #triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 //Business Logic
 function equilateralTriangle()  {
     document.getElementById("result").innerText = "Equlateral";
+    document.getElementById("triangle").style.display = "block";
 }
 function notTriangle()  {
     document.getElementById("result").innerText = "Not a Triangle";
+    document.getElementById("triangle").style.display = "block";
 
 }
 function scaleneTriangle()   {
     document.getElementById("result").innerText = "Scalene Triangle";
+    document.getElementById("triangle").style.display = "block";
 }
 function isoscelesTriangle()   {
-    document.getElementById("result").innerText = "Isosceles Triangle"
+    document.getElementById("result").innerText = "Isosceles Triangle";
+    document.getElementById("triangle").style.display = "block";
 }
 
 function trackTriangle(a, b, c)    {
     //get values of the sides
     
-    if (a === b && a === c)  {
+    if (a < c && b < c && a + b < c)    {
+        "Not a triangle";
+    } else if (a === b && a === c)  {
         equilateralTriangle();
     } else if (a === b || a === c || b === c) {
         isoscelesTriangle();
-    } else if (side1 !== side2 && side2 !== side3  && side1 !== side3)  {
+    } else if (a !== b && b !== c  && a !== c)  {
         scaleneTriangle();
 
     }
@@ -31,9 +37,11 @@ function trackTriangle(a, b, c)    {
 function handleSubmit(event) {
 event.preventDefault();    
 let side1 = document.getElementById("side1").value;
+console.log(side1);
 let side2 = document.getElementById("side2").value;
+console.log(side2);
 let side3 = document.getElementById("side3").value;
-
+console.log(side3);
 let result = trackTriangle(side1, side2, side3);
 document.getElementById("result").innerText = result;
 
